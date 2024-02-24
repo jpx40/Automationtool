60aea1374569af30163256632731e7aa1d778308
Jonas <92295347+jpx40@users.noreply.github.com>
2024-02-24 16:45:07 +0100

    update

--- central/rsrem/src/main.rs
+++ central/rsrem/src/main.rs
@@ -2,6 +2,7 @@ use ssh2::Session;
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::path::Path;
 use toml;
 
 fn main() {}
@@ -9,6 +10,7 @@ fn main() {}
 fn read_config() {}
 
 fn ssh_connect() {
+    // https://docs.rs/ssh2/latest/ssh2/
     let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
     // Try to authenticate with the first identity in the agent.
     let mut sess = Session::new().unwrap();
