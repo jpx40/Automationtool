a4ec79e3bf189a3db4ab5072d348b0cc3eac87a3
Jonas <92295347+jpx40@users.noreply.github.com>
2024-02-24 16:44:15 +0100

    update

--- central/rsrem/src/main.rs
+++ central/rsrem/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use ssh2::Session;
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use toml;
+
+fn main() {}
+
+fn read_config() {}
+
+fn ssh_connect() {
+    let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+    // Try to authenticate with the first identity in the agent.
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password("username", "password").unwrap();
+    assert!(sess.authenticated());
 }
