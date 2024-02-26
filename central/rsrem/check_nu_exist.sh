ec2ef1f209d21b1ba8c9d28b6aa85a2ba5fe407b
Jonas <92295347+jpx40@users.noreply.github.com>
2024-02-26 18:34:28 +0100

    update

--- central/rsrem/check_nu_exist.sh
+++ central/rsrem/check_nu_exist.sh
@@ -17,8 +17,8 @@ case "$os_name" in
 # Add more OS checks as needed
 "Darwin") # macOS
 	# Check Homebrew first
-	if brew list | grep -q "nushell"; then
-		echo "$program_name is installed with Homebrew."
+	if $program_name --version &>/dev/null; then
+		echo "nushell is installed with Homebrew."
 	else
 		# Then check Applications and command-line tools
 		if [ -d "/Applications/$program_name.app" ]; then
@@ -35,12 +35,13 @@ case "$os_name" in
 "Linux") # Linux
 	# Use package manager (replace with your specific command)
 	if dpkg -l "$program_name" | grep -i installed &>/dev/null; then
-		echo "$program_name is installed."
+		# echo "$program_name is installed."
+		echo true
 	else
 		if command -v "$program_name" &>/dev/null; then
-			echo "$program_name is installed."
+			echo true
 		else
-			echo "$program_name is not installed."
+			echo false
 		fi
 	fi
 	;;
